38e708987e78df701ab991e0c33957a682ce7d48
Daniel Huluka <daniel.huluka@nrk.no>
2019-08-22 23:43:43 +0200

    script to deploy the app and ingress config

--- Kubernetes/create-prod.sh
+++ Kubernetes/create-prod.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-namespace=${1:-mdw-test}
+namespace=${1:-mdw-prod}
 echo Using namespace $namespace
+kubectl create -f secret.yaml -n $namespace
 kubectl create -f deployment.yaml -n $namespace
 kubectl create -f service.yaml -n $namespace
+kubectl create -f ingress.yaml -n $namespace
---
+++ Kubernetes/create-test.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+namespace=${1:-mdw-test}
+echo Using namespace $namespace
+kubectl create -f secret.yaml -n $namespace
+kubectl create -f deployment.yaml -n $namespace
+kubectl create -f service.yaml -n $namespace
